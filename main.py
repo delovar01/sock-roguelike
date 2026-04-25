39149031a8cd4b080a5d44fd96bc7178d3f6aa69
Stepan Pavlenko <233904793+delovar01@users.noreply.github.com>
2026-04-25 22:18:00 +0500

    квадратик на экране

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ screen = pygame.display.set_mode((800, 600))
 pygame.display.set_caption("Носок-одиночка")
 clock = pygame.time.Clock()
 
+x = 100
+y = 100
+
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill((30, 30, 40))
+    pygame.draw.rect(screen, (220, 80, 80), (x, y, 32, 32))
     pygame.display.flip()
     clock.tick(60)
 
